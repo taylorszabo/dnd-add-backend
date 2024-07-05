7eb3303a519a0be658eeb2f036ed6d7d2dd470a9
easyseven-vehikl <132284601+easyseven-vehikl@users.noreply.github.com>
2024-07-05 15:05:32 -0400

    add model guarding

--- app/Models/Encounters.php
+++ app/Models/Encounters.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Encounters extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
--- app/Models/Parties.php
+++ app/Models/Parties.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Parties extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
 }
