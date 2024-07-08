0a4b1234fbb8dbfc0bc571270cdae23dec570091
easyseven-vehikl <132284601+easyseven-vehikl@users.noreply.github.com>
2024-07-08 15:30:13 -0400

    create monster factory

--- database/factories/MonstersFactory.php
+++ database/factories/MonstersFactory.php
@@ -13,6 +13,18 @@ class MonstersFactory extends Factory
     public function definition()
     {
         return [
+            'name' => $this->faker->word,
+            'alignment' => $this->faker->randomElement(['Lawful Good', 'Neutral', 'Chaotic Evil']),
+            'size' => $this->faker->randomElement(['Tiny', 'Small', 'Medium', 'Large', 'Huge', 'Gargantuan']),
+            'type' => $this->faker->word,
+            'cr' => $this->faker->numberBetween(0, 30), // Challenge Rating
+            'environment' => $this->faker->word,
+            'source_book' => $this->faker->word,
+            'is_legendary' => $this->faker->boolean,
+            'xp_amount' => $this->faker->numberBetween(10, 50000),
+            'hit_points' => $this->faker->numberBetween(1, 1000),
+            'initiative_modifier' => $this->faker->numberBetween(-5, 5),
+            'is_dead' => $this->faker->boolean,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Monsters;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
+        Monsters::factory(10)->create();
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
